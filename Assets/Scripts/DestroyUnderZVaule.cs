c03e5ac4a86a231c7656cbd95cf9195aea48fe63
ManiacMac <macfglenn@gmail.com>
2015-01-11 17:29:40 -0500

    Pillars and crapo

---
+++ Assets/Scripts/DestroyUnderZVaule.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyUnderZVaule : MonoBehaviour {
+
+	public float destroyValue = 0.0f;
+
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.z <= destroyValue) {
+			Destroy (gameObject);
+		}
+	}
+}
+
