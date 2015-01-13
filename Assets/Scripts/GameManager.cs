b0489c24feb2c12e41c65b36adacd4336f2150e7
ManiacMac <macfglenn@gmail.com>
2015-01-12 20:30:56 -0500

    Score system in place and unified speed

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,11 +3,18 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
+	public static GameManager Instance;
+
+	public float pointsPerUnitTravelled = 1.0f;
+	public float gameSpeed = 10.0f;
+
+	private float score = 0.0f;
+	private static float highScore= 0.0f;
 	private bool gameOver = false;
 
 	// Use this for initialization
 	void Start () {
-	
+		Instance = this;
 	}
 	
 	// Update is called once per frame
@@ -20,9 +27,21 @@ public class GameManager : MonoBehaviour {
 				Application.LoadLevel (Application.loadedLevel);
 			}
 		}
+
+		if (!gameOver) { 
+
+		}
+		score += pointsPerUnitTravelled * gameSpeed * Time.deltaTime;
+		if (score > highScore) {
+			highScore = score;
+		}
 	}
 
 	void OnGUI() {
+		int currentScore = (int)score;
+		int currentHighScore = (int)highScore;
+		GUILayout.Label ("Score " + currentScore.ToString());
+		GUILayout.Label ("HighScore " + currentHighScore.ToString());
 		if (gameOver == true) {
 			GUILayout.Label ("Game Over! Press any key to reset!");
 		}
